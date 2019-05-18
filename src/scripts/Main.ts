48f9915b38516a90c4e1ec18a831feba4426e593
Daar375 <Daar375@gmail.com>
2019-05-17 20:27:17 -0600

    Add files via upload

--- src/scripts/Main.ts
+++ src/scripts/Main.ts
@@ -2,6 +2,7 @@ import { AMOUNT_OF_SONGS, BIT_RATE, BITS,
   LATTER_RATE,  S2_MULTIPLIER } from './library/consts';
 import { getForm, getSector } from './library/sector';
 import { readAudiodata } from './library/wavManagment';
+import {analizeSong} from "./library/Utilities";
 import {Compose} from "./library/Compose";
 const sectorsS1: string[] = ['', ''];
 const sectorsS2: string[] = ['', ''];
@@ -12,4 +13,6 @@ const audioDataS1 = readAudiodata(S1);
 const audioDataS2 = readAudiodata(S2);
 
 
-
+analizeSong(audioDataS1, sectorsS1);
+analizeSong(audioDataS2, sectorsS2);
+console.log(sectorsS1)
--- src/scripts/library/Utilities.ts
+++ src/scripts/library/Utilities.ts
@@ -3,3 +3,14 @@ import {getForm, getSector} from "./sector";
 import {Song} from "../../../build/src/scripts/library/song";
 import {BITS} from "./consts";
 
+export function analizeSong(pAudioData:any, pSectors: string[]) {
+  for (let i = 0; i < pAudioData.channelData[0].length; i += LATTER_RATE) {
+    const channel1 = getSector(pAudioData.channelData[0][i], pAudioData.channelData[0][i + LATTER_RATE]);
+    const channel2 = getSector(pAudioData.channelData[1][i], pAudioData.channelData[1][i + LATTER_RATE]);
+    pSectors[0] += channel1;
+    pSectors[1] += channel2;
+
+  }
+  //pSectors[0] = getForm(pSectors[0]);
+  //pSectors[1] = getForm(pSectors[1]);
+}
