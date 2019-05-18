3aadd564d1cd4bbc3cadc8ca4e2ea702ba33eeed
Daar375 <Daar375@gmail.com>
2019-05-17 20:28:36 -0600

    Add files via upload

--- src/scripts/Main.ts
+++ src/scripts/Main.ts
@@ -6,9 +6,8 @@ import {analizeSong} from "./library/Utilities";
 import {Compose} from "./library/Compose";
 const sectorsS1: string[] = ['', ''];
 const sectorsS2: string[] = ['', ''];
-const S1: string = process.argv[3];
-const S2: string = process.argv[4];
-const command: string = process.argv[2];
+const S1: string = process.argv[2];
+const S2: string = process.argv[3];
 const audioDataS1 = readAudiodata(S1);
 const audioDataS2 = readAudiodata(S2);
 
