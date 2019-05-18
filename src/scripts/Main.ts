2fbd2065c2eb99f106ce38dd46aba8c116a9bf66
Daar375 <Daar375@gmail.com>
2019-05-17 20:21:42 -0600

    Add files via upload

--- src/scripts/Main.ts
+++ src/scripts/Main.ts
@@ -1,10 +1,7 @@
 import { AMOUNT_OF_SONGS, BIT_RATE, BITS,
-  LATTER_RATE, PATTERN_SIZE, S2_MULTIPLIER } from './library/consts';
-import { Pattern } from './library/Patterns';
+  LATTER_RATE,  S2_MULTIPLIER } from './library/consts';
 import { getForm, getSector } from './library/sector';
-import { Song } from './library/song';
 import { readAudiodata } from './library/wavManagment';
-import {analizeSong} from "./library/Utilities";
 import {Compose} from "./library/Compose";
 const sectorsS1: string[] = ['', ''];
 const sectorsS2: string[] = ['', ''];
@@ -15,13 +12,4 @@ const audioDataS1 = readAudiodata(S1);
 const audioDataS2 = readAudiodata(S2);
 
 
-let comp = new Compose();
-analizeSong(audioDataS1, sectorsS1);
-analizeSong(audioDataS2, sectorsS2);
-let SongSize =  sectorsS2.length*10;
 
-const rangesS1 = [comp.ranges(sectorsS1[0]), comp.ranges(sectorsS1[1])];
-const rangesS2 = [comp.ranges(sectorsS2[0]), comp.ranges(sectorsS2[1])];
-let songs:Song[]=[];
-songs=comp.getInitialSongs(rangesS1,rangesS2,SongSize);
-console.log(sectorsS1);
--- src/scripts/library/Utilities.ts
+++ src/scripts/library/Utilities.ts
@@ -3,39 +3,3 @@ import {getForm, getSector} from "./sector";
 import {Song} from "../../../build/src/scripts/library/song";
 import {BITS} from "./consts";
 
-export function getIndex(ranges: any[], rand: number, channel: number) {
-  const letterI = getLetter(ranges, channel, rand);
-  // ranges[channel][3][letterI][ranges[channel][2][letterI][1]-ranges[channel][2][letterI][0]/ranges[channel][3][letterI]]
-  const list: any[] = ranges[channel][3][letterI];
-  return list[rand % list.length];
-}
-
-export function isInRange(lower: number, higher: number, letterNum: number) {
-  return (lower <= letterNum && letterNum <= higher);
-
-}
-export function getLetter(ranges: any[], channel: number, rand: number): number {
-  let found = false;
-  let index = 0;
-  while (!found) {
-    if (isInRange(ranges[channel][2][index][0], ranges[channel][2][index][1], rand)) {
-      found = true;
-    } else {
-      index++;
-    }
-  }
-  // const newIndex = Math.round((ranges[channel][3][index].length - 1) * Math.random());
-  return index;
-}
-export function analizeSong(pAudioData:any, pSectors: string[]) {
-  for (let i = 0; i < pAudioData.channelData[0].length; i += LATTER_RATE) {
-    const channel1 = getSector(pAudioData.channelData[0][i], pAudioData.channelData[0][i + LATTER_RATE]);
-    const channel2 = getSector(pAudioData.channelData[1][i], pAudioData.channelData[1][i + LATTER_RATE]);
-    pSectors[0] += channel1;
-    pSectors[1] += channel2;
-
-  }
-  //pSectors[0] = getForm(pSectors[0]);
-  //pSectors[1] = getForm(pSectors[1]);
-}
-
